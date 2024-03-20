268ee8b5981f3923e48c13a4ba04c4b3cff65871
PeanutBread1 <102591143+PeanutBread1@users.noreply.github.com>
2024-03-20 04:33:16 -0400

    Add files via upload

--- Sudoku.java
+++ Sudoku.java
@@ -1,30 +1,28 @@
 public class Sudoku{        
     
-    public boolean checkBoardEmpty(int board[][], int row, int col){
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[row][col] != 0){
-                    return false;
+    public boolean findEmptyCell(int[][] board, int[] cell) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == 0) {
+                    cell[0] = i;
+                    cell[1] = j;
+                    return true;
                 }
             }
         }
-        return true;
+        return false; //No empty cells found
     }
     
-    public boolean checkCellEmpty(int board[][],int row, int col){
-        if(board[row][col] == 0){
-            return false;
-        }
-        return true;
-    }
-    public boolean checkRow(int board[][], int row, int numbToInsert){ //check if number we are inserting is in the same row
-        for(int i = 0;i < 9; i++){
-            if(board[row][i] == numbToInsert){
+    
+    public boolean checkRow(int[][] board, int row, int num) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == num) {
                 return false;
             }
         }
         return true;
     }
+    
     public boolean checkCol(int board[][], int col, int numbToInsert){//check if number we are inserting is in the same column
         for(int i = 0;i < 9; i++){
             if(board[i][col] == numbToInsert){
@@ -73,20 +71,19 @@ public class Sudoku{
 
         }
     }
-    public boolean check3x3(int board[][], int row, int col, int numbToInsert){//check if number we are inserting is in the same 3x3 grid
-        int grid[][] = markGrid(row, col); //if row = 3, col = 3 grid[][] = {{3,4,5},{3,4,5}}
-
-        for(int i = grid[0][0]; i < i+3; i++){
-            for(int j = grid[1][0]; j < j + 3; j++){
-                if(board[i][j] == numbToInsert){
-                    return false; //numb in 3x3
+    public boolean check3x3(int[][] board, int row, int col, int num) {
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
                 }
             }
-
         }
-        return true; //numb not in 3x3
-
+        return true;
     }
+    
     public boolean isValid(int board[][], int row, int col, int numbToInsert){
         if(checkRow(board, row, numbToInsert ) && checkCol(board, col, numbToInsert) && check3x3(board, row, col, numbToInsert) == true){
             return true;
@@ -94,31 +91,26 @@ public class Sudoku{
             return false;
         }
     }
-    public boolean sudokuRecursion(int board[][], int row, int col){
-        row = 0;
-        col = 0;
-        if(checkBoardEmpty(board, row, col)){ //if board is full return true
-            return true;
-        }else{
-            for(int numbToInsert = 0; numbToInsert < 9; numbToInsert++){//loop through numbers to insert
-                if(checkCellEmpty(board, row, col)&& isValid(board, row, col, numbToInsert)){//check current cell if its empty. if it is and the number we are inserting is valid, insert.
-                    board[row][col] = numbToInsert; //if empty and valid number, insert first valid number
-                    sudokuRecursion(board, row, col);
-                }else if(checkCellEmpty(board, row, col) && isValid(board, row, col, numbToInsert) != true){
-                    if(col == 9){
-                        sudokuRecursion(board, row + 1, 1);
-
-                    }
-                    else{
-                        sudokuRecursion(board, row, col+1);
-                    }
+    public boolean sudokuRecursion(int[][] board) {
+        int[] cell = new int[2]; 
+        if (!findEmptyCell(board, cell)) {
+            return true; 
+        }
+    
+        int row = cell[0], col = cell[1];
+    
+        for (int num = 1; num <= 9; num++) {
+            if (isValid(board, row, col, num)) {
+                board[row][col] = num;
+                
+                if (sudokuRecursion(board)) {
+                    return true; 
                 }
-                else {
-                    board[row][col] = 0;
-                    return false;
-                }
+    
+                board[row][col] = 0; 
             }
         }
-        return false;
+        return false; 
     }
+    
 }
